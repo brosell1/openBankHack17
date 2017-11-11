1ca4f284a0b2dcc7517775a99f54f4bc8a44b1a0
Christopher Miller <ccmiller2018@gmail.com>
2017-11-11 02:52:36 +0000

    tired won

---
+++ express/routes/staff.js
@@ -0,0 +1,49 @@
+const Staff = require(`../models/StaffModel.js`);
+const express = require(`express`);
+const router = express.Router();
+
+/* GET users listing. */
+router.get(`/`, (req, res) => {
+  Staff.find(function(err, response) {
+    if(err) {
+      res.json({error:err});
+    }
+    res.json({payload:response});
+  });
+});
+
+router.post('/', (req, res) => {
+    var newstaff = new Staff();
+    newstaff.username = req.body.username;
+    newstaff.password = req.body.password;
+    newstaff.companyId = req.body.companyId;
+    newstaff.authLevel = req.body.authLevel;
+    newstaff.admin = req.body.admin;
+    newstaff.save(function(err, response){
+      if(err) {
+        res.json({error:err});
+      }
+      res.json({payload:response});
+    })
+});
+
+router.get('/:username', (req, res) => {
+  Staff.findOne({username:req.params.username}, function(err, response) {
+    if(err) {
+      res.json({error:err});
+    }
+    res.json({payload:response});
+  });
+});
+
+router.put('/:username', (req, res) => {
+    newstaff.findOneAndUpdate({username:req.params.username}, req.body, function(err, response){
+      if(err) {
+        res.json({error:err});
+      }
+      res.json({payload:response});
+    })
+});
+
+
+module.exports = router;
