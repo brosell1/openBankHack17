48ba1eb1f477ddc16d33c40d2476cc3cac7e3e09
maddocash <danksashleigh@gmail.com>
2017-11-12 02:14:14 +0000

    all pages made

--- frontend/src/components/NewTeamMember/index.js
+++ frontend/src/components/NewTeamMember/index.js
@@ -5,7 +5,10 @@ class NewTeamMember extends Component {
   render() {
     return (
       <div className="NewTeamMember">
-        <Notifications handleReturn={this.props.handleReturn} handleLogoutClick={this.handleLogoutClick} pageTitle="New Team Member"/>
+        <Notifications handleReturn={this.props.handleReturn} handleLogoutClick={this.handleLogoutClick} pageTitle="New Beneficiary"/>
+        <input type='email'/>
+        <input type='text'/>
+        <button>Create Team Member</button>
       </div>
     );
   }
