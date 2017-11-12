6b1195a14e0587bfe0dad1a20aefa1470c2bc80b
maddocash <danksashleigh@gmail.com>
2017-11-12 05:05:37 +0000

    home buttons nice

--- frontend/src/components/NewTeamMember/index.js
+++ frontend/src/components/NewTeamMember/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import Notifications from '../Notifications'
+import Notifications from '../Notifications';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+    padding: '35vh',
+    color:'black'
+  }
+};
 
 class NewTeamMember extends Component {
+
   render() {
     return (
       <div className="NewTeamMember">
         <Notifications handleReturn={this.props.handleReturn} handleLogoutClick={this.props.handleLogoutClick} pageTitle="New Team Member"/>
-        <input type='email'/>
-        <input type='text'/>
-        <input type='checkbox' />
-        <button>Create Team Member</button>
+          <TextField hintText="Email" />
+          <TextField hintText="Password" />
+          <RaisedButton ladel='Create Team Member' type='checkbox' labelColor='black'/>
       </div>
     );
   }
