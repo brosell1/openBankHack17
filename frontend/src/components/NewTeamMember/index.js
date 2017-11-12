7c76259cb2e87e04c31a2d38492fc31dd43dcfa5
Petekachu <peteryu.uk@gmail.com>
2017-11-12 11:00:30 +0000

    buttons rearranged and linked

--- frontend/src/components/NewTeamMember/index.js
+++ frontend/src/components/NewTeamMember/index.js
@@ -28,7 +28,7 @@ class NewTeamMember extends Component {
         <div style={containerStyle}>
           <TextField type='email' hintText="Username" style={styles}/>
           <TextField type='text' hintText="Password" style={styles}/>
-          <Checkbox label="Is he admin?" style={styles}/>
+          <Checkbox label="Admin" style={styles}/>
           <RaisedButton fullWidth backgroundColor="#c34433" labelColor="#fff" label="Create" style={buttonStyles} />
         </div>
       </div>
